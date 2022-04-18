40303798a332d386ce8b8242fd5175ccabdf5305
Ronan Salvador <ronansalvador@yahoo.com.br>
2022-04-18 19:15:07 -0300

    Requisito 5

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,11 +2,59 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 class Search extends Component {
+  constructor() {
+    super();
+    this.state = {
+      busca: '',
+      button: true,
+
+    };
+  }
+
+  inputSearch = ({ target }) => {
+    this.setState({
+      busca: target.value,
+    }, () => {
+      this.buttonCheck();
+    });
+  }
+
+  buttonCheck = () => {
+    const { busca } = this.state;
+    if (busca.length > 1) {
+      this.setState({
+        button: false,
+      });
+    } else {
+      this.setState({
+        button: true,
+      });
+    }
+  }
+
   render() {
+    const { busca, button } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        Search
+        <form>
+          <input
+            type="text"
+            data-testid="search-artist-input"
+            placeholder="Pesquisa"
+            value={ busca }
+            onChange={ this.inputSearch }
+          />
+          <button
+            type="button"
+            data-testid="search-artist-button"
+            value="Nome"
+            onClick={ () => console.log('button searsh') }
+            disabled={ button }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
