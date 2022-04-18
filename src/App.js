3e68635a1c2d9afbb6c7dbda172ff7b237722d0d
Ronan Salvador <ronansalvador@yahoo.com.br>
2022-04-18 15:17:25 -0300

    Requisito 1 - rotas

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Search from './pages/Search';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import NotFound from './pages/NotFound';
 // iniciando o projeto
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <div>
+        <p>TrybeTunes</p>
+        <Route path="/" component={ Login } exact />
+        <Route path="/search" component={ Search } exact />
+        <Route path="/album/:id" component={ Album } exact />
+        <Route path="/favorites" component={ Favorites } exact />
+        <Route path="/profile" component={ Profile } exact />
+        <Route path="/profile/edit" component={ ProfileEdit } exact />
+        <Route path="*" component={ NotFound } />
+      </div>
+    );
   }
 }
 
